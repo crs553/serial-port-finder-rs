1654c0a858ad6fe7707082c2aa1f3070a0f70620
crs553 <42813301+crs553@users.noreply.github.com>
2025-05-24 19:41:14 +0100

    feat: list serial ports

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use serialport::SerialPortInfo;
+
+fn main() {
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{}", p.port_name);
+    }
+}
